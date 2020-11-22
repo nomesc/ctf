da8946397c3c5a384a9a6f3b70af5712625615e4
calin <calin@calin-mac.local>
2020-11-22 18:28:44 +0200

    Add infoleak bug

--- server/src/service_population.c
+++ server/src/service_population.c
@@ -18,9 +18,9 @@ static void service_population_init()
     population = malloc(population_array_size * sizeof(struct country_population));
     for (int i = 0; i < population_array_size; i++)
     {
-        char * country_name[64];
+        char country_name[64];
         int country_pop;
-        fscanf(f, '%s', country_name);
+        fscanf(f, "%63s", country_name);
         fscanf(f, "%d", &country_pop);
         population[i].country_name = malloc((strlen(country_name) + 1) * sizeof(char));
         population[i].country_name = country_name;
@@ -37,7 +37,9 @@ void *service_population_cb(struct request *req)
 
 int handle_service_population(struct request *req)
 {
-    uint64_t population;
+    uint64_t country_population;
+    uint64_t i;
+    struct service_population *service = (struct service_population *)req->service;
     if (population != NULL)
     {
         pthread_mutex_lock(&service_population_init_mutex);
@@ -45,7 +47,20 @@ int handle_service_population(struct request *req)
             service_population_init();
         pthread_mutex_unlock(&service_population_init_mutex);
     }
-
-    send(req->client_connection, "POPULATION\n", 12, 0);
-    return 1;
+    if(population_array_size == 0)
+    {
+        send(req->client_connection, "ERR No info for this country yet...\n", 37, 0);
+        return -1;
+    }
+    i = 0;
+    country_population = (uint64_t)&population;
+    while(i < population_array_size)
+    {
+        if(strncmp(population[i].country_name, service->country_name, 64) == 0 && population[i].populatie > 0)
+            country_population = population[i].populatie;
+    }
+    char *raspuns = malloc(64);
+    sscanf(raspuns, "%llu\n", country_population);
+    send(req->client_connection, raspuns, strlen(raspuns), 0);
+    return 0;
 }
