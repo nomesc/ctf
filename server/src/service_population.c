ff7ab7dbfc4bb606d78dfbde3421354f0d2ef5e4
calin <calin@calin-mac.local>
2020-11-22 17:27:09 +0200

    Start population service functionality

--- server/src/service_population.c
+++ server/src/service_population.c
@@ -1,5 +1,15 @@
 #include "service_population.h"
 
+struct country_population
+{
+    char *country_name;
+    int populatie;
+};
+
+static struct country_population *population = NULL;
+
+// functie statica service_population_init()
+
 void *service_population_cb(struct request *req)
 {
     free(((struct service_population *)(req->service))->country_name);
